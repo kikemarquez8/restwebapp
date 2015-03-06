fb281684cdece3a2e0741a0a1f6844db4123d8e5
Bonsanto <xbonsx@gmail.com>
2015-03-05 21:58:32 -0430

    - Added insert menu for products

--- web/realrestclient/controller/inventoryController.js
+++ web/realrestclient/controller/inventoryController.js
@@ -1,4 +1,3 @@
-var dataStored = document.querySelectorAll("input");
 var request = {
 	"method": "POST",
 	"url": "http://localhost:9998/product/create",
@@ -16,12 +15,9 @@ var request = {
 angular.module('angularApp', [])
 	.controller('InventoryController', ['$scope', '$http',
 		function ($scope, $http) {
+
 			$scope.sendProduct = function () {
-				request.data.id_product = document.getElementById('id').value + "";
-				request.data.na_product = document.getElementById('name').value + "";
-				request.data.pp_product = document.getElementById('purchasePrice').value + "";
-				request.data.sp_product = document.getElementById('salePrice').value + "";
-				request.data.qt_product = document.getElementById('quantity').value + "";
+				readForm();
 				console.log(request.data);
 				$http(request).success(function (data) {
 					alert(data.message + ", the product was inserted");
@@ -29,5 +25,44 @@ angular.module('angularApp', [])
 				}).error(function (error) {
 					alert(error.message + ", the product couldn't be inserted");
 				});
+			};
+
+			$scope.testID = function () {
+				var id = document.querySelector("#id");
+				id.value = parseInt(id.value);
+				document.getElementById('id').style.backgroundColor = "white";
+
+				if (id.value !== null && id.value !== "") {
+					$http.get("http://localhost:9998/product/" + id.value).
+						success(function (data) {
+
+							//if the product already exists.
+							if (data.message === undefined) {
+								id.style.backgroundColor = "#d14";
+							} else {
+								id.style.backgroundColor = "steelblue";
+								id.style.color = "white";
+							}
+						}).error(function (data) {
+							alert(data.message);
+						});
+				}
+			};
+
+			$scope.clearField = function () {
+				document.getElementById('id').style.backgroundColor = "white";
+				document.getElementById('id').value = "";
+				document.getElementById('name').value = "";
+				document.getElementById('purchasePrice').value = "";
+				document.getElementById('salePrice').value = "";
+				document.getElementById('quantity').value = "";
+			};
+
+			var readForm = function () {
+				request.data.id_product = document.getElementById('id').value + "";
+				request.data.na_product = document.getElementById('name').value + "";
+				request.data.pp_product = document.getElementById('purchasePrice').value + "";
+				request.data.sp_product = document.getElementById('salePrice').value + "";
+				request.data.qt_product = document.getElementById('quantity').value + "";
 			};
 		}]);
